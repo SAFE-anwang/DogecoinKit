24f036e1ca255983f6c1b2cf4cbe4cf7d1ee6a46
JiangSonglun <271398741@qq.com>
2023-11-19 02:00:49 +0800

    add DogecoinKit package

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "DogecoinKit",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "DogecoinKit",
+            targets: ["DogecoinKit"]),
+    ], 
+
+    dependencies: [
+        .package(url: "https://github.com/SAFE-anwang/BitcoinCore.Swift.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/greymass/swift-scrypt.git", from: "1.0.0"),
+        .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "DogecoinKit",
+            dependencies: [
+                .product(name: "Scrypt", package: "swift-scrypt"),
+                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
+                .product(name: "HsToolKit", package: "HsToolKit.Swift"),
+            ]
+        ),
+    ]
+)
---
+++ iOS Example/Sources/Adapters/DogecoinAdapter.swift
@@ -0,0 +1,48 @@
+import DogecoinKit
+import BitcoinCore
+import HsToolKit
+import HdWalletKit
+
+class DogecoinAdapter: BaseAdapter {
+    let dogecoinKit: Kit
+
+    init(words: [String], purpose: Purpose, testMode: Bool, syncMode: BitcoinCore.SyncMode, logger: Logger) {
+        let networkType: Kit.NetworkType = testMode ? .testNet : .mainNet
+        guard let seed = Mnemonic.seed(mnemonic: words) else {
+            fatalError("Can't Create Seed")
+        }
+
+        dogecoinKit = try! Kit(seed: seed, purpose: purpose, walletId: "walletId", syncMode: syncMode, networkType: networkType, confirmationsThreshold: 1, logger: logger.scoped(with: "DogecoinKit"))
+
+        super.init(name: "Dogecoin", coinCode: "dgoecoin", abstractKit: dogecoinKit)
+        dogecoinKit.delegate = self
+    }
+
+    class func clear() {
+        try? Kit.clear()
+    }
+}
+
+extension DogecoinAdapter: BitcoinCoreDelegate {
+
+    func transactionsUpdated(inserted: [TransactionInfo], updated: [TransactionInfo]) {
+        transactionsSubject.send()
+    }
+
+    func transactionsDeleted(hashes: [String]) {
+        transactionsSubject.send()
+    }
+
+    func balanceUpdated(balance: BalanceInfo) {
+        balanceSubject.send()
+    }
+
+    func lastBlockInfoUpdated(lastBlockInfo: BlockInfo) {
+        lastBlockSubject.send()
+    }
+
+    public func kitStateUpdated(state: BitcoinCore.KitState) {
+        syncStateSubject.send()
+    }
+
+}
