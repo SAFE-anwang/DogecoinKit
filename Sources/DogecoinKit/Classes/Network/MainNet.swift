dd5a0836260e8a1de7710329aa8ac99d16a33262
JiangSonglun <271398741@qq.com>
2023-11-21 17:29:04 +0800

    fix bug

--- Sources/DogecoinKit/Classes/Network/MainNet.swift
+++ Sources/DogecoinKit/Classes/Network/MainNet.swift
@@ -2,10 +2,11 @@ import BitcoinCore
 
 public class MainNet: INetwork {
     public let bundleName = "Dogecoin"
-
-    public let pubKeyHash: UInt8 = 0x30
+    
+    public let protocolVersion: Int32 = 70015
+    public let pubKeyHash: UInt8 = 0x1e //  => addressVersion
     public let privateKey: UInt8 = 0xb0
-    public let scriptHash: UInt8 = 0x32
+    public let scriptHash: UInt8 = 0x16
     public let bech32PrefixPattern: String = "doge"
     public let xPubKey: UInt32 = 0xfdcafa02// 0x02facafd
     public let xPrivKey: UInt32 = 0x98c3fa02// 0x02fac398
@@ -26,3 +27,4 @@ public class MainNet: INetwork {
     public init() {}
 
 }
+
